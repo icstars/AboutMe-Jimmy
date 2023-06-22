f547b230acfc41647646a87af6de5efaa80ebb7a
muayengo <136016343+muayengo@users.noreply.github.com>
2023-06-22 10:21:56 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// const name = prompt("what is your name?");
+
+// const age = prompt("How old are you?");
+
+// const hobby = prompt("What is your hobby?");
+
+// alert(
+//   `Hi, my name is ${name}, I am ${age} years old, and I like playing ${hobby}`
+// );
